35c8723163d9c8d5d836d231e8de3757a01f8710
David Ashman <david.ashman@interfolio.com>
2023-12-03 23:26:28 -0500

    Status debug

--- index.js
+++ index.js
@@ -404,7 +404,7 @@ class LightBulb {
     }
 
     updateStatus(isOn, brightness, colorTemp, rgb) {
-        this.log.info(`Updating switch ID ${this.switchID}, meshID ${this.meshID} - on? ${isOn}, brightness ${brightness}, temp ${colorTemp}`);
+        this.log.info(`Updating switch ID ${this.switchID}, meshID ${this.meshID} - on? ${isOn}, brightness ${brightness}, temp ${colorTemp}, rgb ${JSON.stringify(rgb)}`);
         this.on = isOn;
         this.brightness = brightness;
         this.colorTemp = colorTemp;
