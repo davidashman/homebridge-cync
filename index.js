4a1d35bc5cc459d54409e097803c6c842da6b473
David Ashman <david.ashman@interfolio.com>
2023-12-05 20:11:28 -0500

    Use standard update

--- index.js
+++ index.js
@@ -510,22 +510,13 @@ class LightBulb {
         request.writeUInt8(this.rgb[2], 13);
         request.writeUInt8((496 + this.meshID + (this.on ? 1 : 0) + this.brightness + this.cyncColorTemp + this.rgb[0] + this.rgb[1] + this.rgb[2]) % 256, 14);
         request.writeUInt8(0x7e, 15);
-        this.log.info(`Sending full update for ${this.displayName}: ${request.toString('hex')}`);
+        this.log.info(`Sending update for ${this.name}: ${request.toString('hex')}`);
         this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATE, request, true);
     }
 
     setOn(value) {
         this.on = value;
         this.sendUpdate();
-        // const request = Buffer.alloc(13);
-        // request.writeUInt16BE(this.meshID, 3);
-        // request.writeUInt8(PACKET_SUBTYPE_SET_STATUS, 5);
-        // request.writeUInt8(this.on, 8);
-        // request.writeUInt8((429 + this.meshID + (this.on ? 1 : 0)) % 256, 11);
-        // request.writeUInt8(0x7e, 12);
-        //
-        // this.log.info(`Sending status update for ${this.displayName}: ${request.toString('hex')}`);
-        // this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATUS, request, true);
     }
 
     setBrightness(value) {
@@ -537,16 +528,6 @@ class LightBulb {
         this.colorTemp = value;
         this.cyncColorTemp = 100 - Math.round(((this.colorTemp - 140) * 100) / 360);
         this.sendUpdate();
-        // const request = Buffer.alloc(12);
-        // request.writeUInt16BE(this.meshID, 3);
-        // request.writeUInt8(PACKET_SUBTYPE_SET_COLOR_TEMP, 5);
-        // request.writeUInt8(0x05, 8);
-        // request.writeUInt8(this.cyncColorTemp, 9);
-        // request.writeUInt8((469 + this.meshID + this.cyncColorTemp) % 256, 10);
-        // request.writeUInt8(0x7e, 11);
-        //
-        // this.log.info(`Sending color temp update for ${this.displayName}: ${request.toString('hex')}`);
-        // this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_COLOR_TEMP, request, true);
     }
 
     setRGB() {
