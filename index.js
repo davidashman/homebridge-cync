698416c8d4580b1ccab7a97410db2fb5e366f540
David Ashman <david.ashman@interfolio.com>
2023-12-05 20:24:23 -0500

    Debug brightness

--- index.js
+++ index.js
@@ -516,50 +516,52 @@ class LightBulb {
 
     setOn(value) {
         this.on = value;
-        const request = Buffer.alloc(13);
-        request.writeUInt16BE(this.meshID, 3);
-        request.writeUInt8(PACKET_SUBTYPE_SET_STATUS, 5);
-        request.writeUInt8(this.on, 8);
-        request.writeUInt8((429 + this.meshID + (this.on ? 1 : 0)) % 256, 11);
-        request.writeUInt8(0x7e, 12);
-
-        this.log.info(`Sending status update for ${this.name}: ${request.toString('hex')}`);
-        this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATUS, request, true);
+        this.sendUpdate();
+        // const request = Buffer.alloc(13);
+        // request.writeUInt16BE(this.meshID, 3);
+        // request.writeUInt8(PACKET_SUBTYPE_SET_STATUS, 5);
+        // request.writeUInt8(this.on, 8);
+        // request.writeUInt8((429 + this.meshID + (this.on ? 1 : 0)) % 256, 11);
+        // request.writeUInt8(0x7e, 12);
+        //
+        // this.log.info(`Sending status update for ${this.name}: ${request.toString('hex')}`);
+        // this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATUS, request, true);
     }
 
     setBrightness(value) {
         this.brightness = value;
         this.sendUpdate();
-
-        const request = Buffer.alloc(16);
-        request.writeUInt16BE(this.meshID, 3);
-        request.writeUInt8(PACKET_SUBTYPE_SET_STATE, 5);
-        request.writeUInt8(this.on, 8);
-        request.writeUInt8(this.brightness, 9);
-        request.writeUInt8(this.cyncColorTemp, 10);
-        request.writeUInt8(this.rgb[0], 11);
-        request.writeUInt8(this.rgb[1], 12);
-        request.writeUInt8(this.rgb[2], 13);
-        request.writeUInt8((496 + this.meshID + (this.on ? 1 : 0) + this.brightness + this.cyncColorTemp + this.rgb[0] + this.rgb[1] + this.rgb[2]) % 256, 14);
-        request.writeUInt8(0x7e, 15);
-
-        this.log.info(`Sending brightness update: ${request.toString('hex')}`);
-        this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATE, request, true);
+        //
+        // const request = Buffer.alloc(16);
+        // request.writeUInt16BE(this.meshID, 3);
+        // request.writeUInt8(PACKET_SUBTYPE_SET_STATE, 5);
+        // request.writeUInt8(this.on, 8);
+        // request.writeUInt8(this.brightness, 9);
+        // request.writeUInt8(this.cyncColorTemp, 10);
+        // request.writeUInt8(this.rgb[0], 11);
+        // request.writeUInt8(this.rgb[1], 12);
+        // request.writeUInt8(this.rgb[2], 13);
+        // request.writeUInt8((496 + this.meshID + (this.on ? 1 : 0) + this.brightness + this.cyncColorTemp + this.rgb[0] + this.rgb[1] + this.rgb[2]) % 256, 14);
+        // request.writeUInt8(0x7e, 15);
+        //
+        // this.log.info(`Sending brightness update: ${request.toString('hex')}`);
+        // this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_STATE, request, true);
     }
 
     setColorTemp(value) {
         this.colorTemp = value;
         this.cyncColorTemp = 100 - Math.round(((this.colorTemp - 140) * 100) / 360);
-        const request = Buffer.alloc(12);
-        request.writeUInt16BE(this.meshID, 3);
-        request.writeUInt8(PACKET_SUBTYPE_SET_COLOR_TEMP, 5);
-        request.writeUInt8(0x05, 8);
-        request.writeUInt8(this.cyncColorTemp, 9);
-        request.writeUInt8((469 + this.meshID + this.cyncColorTemp) % 256, 10);
-        request.writeUInt8(0x7e, 11);
-
-        this.log.info(`Sending color temp update for ${this.name}: ${request.toString('hex')}`);
-        this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_COLOR_TEMP, request, true);
+        this.sendUpdate();
+        // const request = Buffer.alloc(12);
+        // request.writeUInt16BE(this.meshID, 3);
+        // request.writeUInt8(PACKET_SUBTYPE_SET_COLOR_TEMP, 5);
+        // request.writeUInt8(0x05, 8);
+        // request.writeUInt8(this.cyncColorTemp, 9);
+        // request.writeUInt8((469 + this.meshID + this.cyncColorTemp) % 256, 10);
+        // request.writeUInt8(0x7e, 11);
+        //
+        // this.log.info(`Sending color temp update for ${this.name}: ${request.toString('hex')}`);
+        // this.hub.sendRequest(PACKET_TYPE_STATUS, this.switchID, PACKET_SUBTYPE_SET_COLOR_TEMP, request, true);
     }
 
     setRGB() {
